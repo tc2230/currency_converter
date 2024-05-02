61579be0dda658cc89697be04abeac400bcbf48f
Tyrael Fu <tyraelfu@TyraeldeMacBook-Pro.local>
2024-05-02 22:45:57 +0800

    implement amount validation function

--- api.py
+++ api.py
@@ -3,18 +3,39 @@
 @author: taylor.fu
 """
 from fastapi import FastAPI
+import re
 
 app = FastAPI()
 
 class CurrencyExchangeService:
     def __init__(self):
-        pass
-
-    def round(self, amount, places):
-        pass
+        self.rate_reference = {
+            "currencies": {
+                "TWD": {
+                    "TWD": 1,
+                    "JPY": 3.669,
+                    "USD": 0.03281
+                },
+                "JPY": {
+                    "TWD": 0.26956,
+                    "JPY": 1,
+                    "USD": 0.00885
+                },
+                "USD": {
+                    "TWD": 30.444,
+                    "JPY": 111.801,
+                    "USD": 1
+                }
+            }
+        }
 
     def validate_amount(self, amount):
-        pass
+        # validate input amount w/ or w/o separator 
+        if ',' in amount:
+            pattern = r"^([1-9]\d{0,2}|0)(,\d{3})*(\.\d+)?$"
+        else:
+            pattern = r"^([1-9]\d*|0)(\.\d+)?$"
+        return True if re.match(pattern, amount) else False
     
     def convert(self, source, target, amount):
         pass
